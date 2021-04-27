3f8af8eaab38993dbf2a6146d92f4e76cdda8ac1
Sa <sjgielis@gmail.com>
2021-04-26 22:52:58 -0300

    create/se sube maquetado seccion reportes

--- js/app.js
+++ js/app.js
@@ -6,3 +6,36 @@ burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('is-active')
     navbarBasicExample.classList.toggle('is-active')
 })
+
+
+const btnReport = document.getElementById("btn-reports");
+const btnCategories = document.getElementById("btn-categories");
+const btnBalance = document.getElementById("btn-balance");
+const reportSection = document.getElementById("report-section");
+const formOperation = document.getElementById("form-operation");
+const balanceSection = document.getElementById("balance-section");
+
+
+
+btnReport.addEventListener("click", () => {
+    
+    balanceSection.classList.add("is-hidden");
+    formOperation.classList.add("is-hidden");
+    reportSection.classList.remove("is-hidden");
+  });
+
+
+  btnBalance.addEventListener('click', () => {
+    balanceSection.classList.remove('is-hidden')
+    reportSection.classList.add('is-hidden')
+    // categoriesSection.classList.add('is-hidden')
+    formOperation.classList.add('is-hidden')
+});
+
+btnCategories.addEventListener('click', () => {
+    balanceSection.classList.add('is-hidden')
+    reportSection.classList.add('is-hidden')
+    // categoriesSection.classList.remove('is-hidden')
+    formOperation.classList.add('is-hidden')
+});
+
